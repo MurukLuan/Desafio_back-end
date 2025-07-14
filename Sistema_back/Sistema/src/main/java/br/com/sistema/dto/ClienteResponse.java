a10fe847189c44a7ae802751b6a13d5cd710f3e9
Luan Muruk Ferreira tonaco <luan.tonaco@trabalho.gov.br>
2025-07-14 10:57:09 -0300

    adicionando alguns testes

--- Sistema_back/Sistema/src/main/java/br/com/sistema/dto/ClienteResponse.java
+++ Sistema_back/Sistema/src/main/java/br/com/sistema/dto/ClienteResponse.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import br.com.sistema.entity.Cliente;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ClienteResponse {
 
